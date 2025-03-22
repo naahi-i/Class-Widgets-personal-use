ee04a8cfe35d93e579e812b352e2fe47ffa6c87b
naahi-i <2228055359@qq.com>
2025-03-22 22:32:35 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -303,7 +303,6 @@ class PluginCard(CardWidget):  # 插件卡片
         self.moreButton = TransparentDropDownToolButton()
         self.moreMenu = RoundMenu(parent=self.moreButton)
         self.settingsBtn = TransparentToolButton()  # 设置按钮
-        self.settingsBtn.hide()
 
         self.hBoxLayout = QHBoxLayout(self)
         self.hBoxLayout_Title = QHBoxLayout(self)
@@ -320,12 +319,14 @@ class PluginCard(CardWidget):  # 插件卡片
             )
         ])
 
+        if enable_settings:
+            self.moreMenu.addSeparator()
+            self.moreMenu.addAction(Action(fIcon.SETTING, f'“{title}”插件设置', triggered=self.show_settings))
+        else:
+            self.settingsBtn.hide()
+
         if plugin_dir in enabled_plugins['enabled_plugins']:  # 插件是否启用
             self.enableButton.setChecked(True)
-            if enable_settings:
-                self.moreMenu.addSeparator()
-                self.moreMenu.addAction(Action(fIcon.SETTING, f'“{title}”插件设置', triggered=self.show_settings))
-                self.settingsBtn.show()
 
         self.setFixedHeight(73)
         self.iconWidget.setFixedSize(48, 48)
